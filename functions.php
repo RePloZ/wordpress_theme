d3bd338dbb8da7a45df5e2d88729f91bdecc332a
Hervé Kay <herve.kay@protonmail.com>
2024-06-03 00:59:49 +0200

    feat(css): add functional tailwind_css + alpine_js + swiper_js

--- functions.php
+++ functions.php
@@ -1,8 +1,10 @@
 <?php
 function enqueue_tailwind_css() {
     wp_enqueue_style('style', get_stylesheet_uri() );
-    wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css')
-    wp_enqueue_script('swiper-js', 'https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.8.2/alpine.min.js')
+    wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css');
+    wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js');
+    wp_enqueue_script('alpine-js', 'https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.8.2/alpine.min.js');
+    wp_enqueue_script('tailwind-js', 'https://cdn.tailwindcss.com');
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_tailwind_css');
